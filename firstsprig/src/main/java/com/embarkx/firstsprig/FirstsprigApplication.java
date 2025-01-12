a23b840a6f21847104157cd97ee6efe1826c65f7
yadavpdsagar <130287779+yadavpdsagar@users.noreply.github.com>
2025-01-12 23:32:05 +0545

    Save local changes before merging

---
+++ firstsprig/src/main/java/com/embarkx/firstsprig/FirstsprigApplication.java
@@ -0,0 +1,13 @@
+package com.embarkx.firstsprig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstsprigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstsprigApplication.class, args);
+	}
+
+}
---
+++ firstsprig/src/test/java/com/embarkx/firstsprig/FirstsprigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.embarkx.firstsprig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstsprigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
